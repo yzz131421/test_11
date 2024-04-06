2ae46c0eb63a60451d1515f9d49dd2299c26c830
yzz <209349050@qq.com>
2024-04-06 16:32:00 +0800

    second commit

--- src/com/hspedu/test_git/git.java
+++ src/com/hspedu/test_git/git.java
@@ -3,5 +3,6 @@ package com.hspedu.test_git;
 public class git {
     public static void main(String[] args) {
         System.out.println("要添加到git喽");
+        System.out.println("第二次添加git");
     }
 }
