f255e77d26e27d96fef2f1c26b95ad67732f18dd
yzz <209349050@qq.com>
2024-04-06 22:23:11 +0800

    hot-fix commit2

--- src/com/hspedu/test_git/git.java
+++ src/com/hspedu/test_git/git.java
@@ -2,8 +2,9 @@ package com.hspedu.test_git;
 
 public class git {
     public static void main(String[] args) {
-        System.out.println("要添加到git喽");
-        System.out.println("第二次添加git");
-        System.out.println("第3次添加git");
+        System.out.println("第1次添加到git");
+        System.out.println("第2次添加到git");
+        System.out.println("第3次添加到git");
+        System.out.println("hot-fix commit git");
     }
 }
